4074b413c9467d42c1548f2b8ce814264e749de5
TYChisYT <chiscalovkaivan@gmail.com>
2023-03-21 16:25:05 +0100

    Lab3

--- script.js
+++ script.js
@@ -20,3 +20,27 @@ console.log("Wynik odejmowania to: "
 document.getElementById('addR').
 innerHTML = add;
 document.getElementById('subR').innerHTML = 'Wynik odejmowania '+a+' i '+b+' to '+sub; 
+
+/*
+    Zadanie 2
+*/
+
+let s1=3;
+let s2=4;
+let s3=5;
+// powinno wyjść 6
+
+// a)
+let p = (s1+s2+s3) /2;
+let area = Math.sqrt(p*(p-s1)*(p-s2)*(p-s3))
+console.log(area);
+
+// b) wynik + wyjaśnienia
+document.getElementById('strony').innerHTML = 'Dla 3-kąta z stronami a, b i c pole ='+area;
+
+
+/*
+    Zadania 3 - losowanie liczb
+*/
+
+
